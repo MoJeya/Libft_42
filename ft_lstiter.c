3c26954512d3eca5c3c8b92693f354e72c16e81f
Moghanavathanan Jeyavathanan <mjeyavat@1-C-5.42heilbronn.de>
2021-07-09 11:06:56 +0200

    these are linked list functions

---
+++ ft_lstiter.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/09 09:51:38 by mjeyavat          #+#    #+#             */
+/*   Updated: 2021/07/09 09:58:19 by mjeyavat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list *tmp;
+
+	if (!lst)
+		return ((void)NULL);
+	while (lst)
+	{	tmp = lst;
+		f(tmp->content);
+		lst =  lst->next;
+	}
+}
