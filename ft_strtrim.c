5e7e357551d72b55a05993a116c18f9ecdc6b59f
Moghanavathanan Jeyavathanan <mjeyavat@1-D-5.42heilbronn.de>
2021-07-21 16:01:31 +0200

    Csegmantaition fault bug Fixed also refactored Code

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,57 +6,91 @@
 /*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 13:27:02 by mjeyavat          #+#    #+#             */
-/*   Updated: 2021/07/16 12:19:43 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2021/07/21 15:57:49 by mjeyavat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+/**
+ * /////OLD CODE!/////OLD CODE!/////OLD CODE!/////OLD CODE!/////OLD CODE!///////*
+*/
+//static size_t	ft_findP_start(char const *s1, char const *set)
+//{
+//	size_t	cnt;
 
-static size_t	ft_findP_start(char const *s1, char const *set)
-{
-	size_t	cnt;
+//	cnt = 0;
+//	while ((cnt < ft_strlen(s1) && (s1[cnt] != '\0')))
+//	{
+//		if (ft_strchr(set, s1[cnt]))
+//			cnt++;
+//		else
+//			break ;
+//	}
+//	return (cnt);
+//}
 
-	cnt = 0;
-	while ((cnt < ft_strlen(s1) && (s1[cnt] != '\0')))
-	{
-		if (ft_strchr(set, s1[cnt]))
-			cnt++;
-		else
-			break ;
-	}
-	return (cnt);
-}
+//static size_t	ft_findP_end(char const *s1, char const *set)
+//{
+//	size_t	cnt;
 
-static size_t	ft_findP_end(char const *s1, char const *set)
-{
-	size_t	cnt;
+//	cnt = 0;
+//	while (s1[cnt] != '\0')
+//		cnt++;
+//	while ((cnt > 0) && (s1[cnt - 1] != '\0'))
+//	{
+//		if (ft_strchr(set, s1[cnt - 1]))
+//			cnt--;
+//		else
+//			break ;
+//	}
+//	return (cnt);
+//}
 
-	cnt = 0;
-	while (s1[cnt] != '\0')
-		cnt++;
-	while ((cnt > 0) && (s1[cnt - 1] != '\0'))
-	{
-		if (ft_strchr(set, s1[cnt - 1]))
-			cnt--;
-		else
-			break ;
-	}
-	return (cnt);
-}
 
+//char	*ft_strtrim(char const *s1, char const *set)
+//{
+//	size_t	tmpStart;
+//	size_t	tmpEnd;
+
+//	tmpStart = 0;
+//	tmpEnd = 0;
+//	if (!s1)
+//		return (NULL);
+//	if (ft_strlen(set) == 0)
+//		return ((char *)s1);
+//	tmpStart = ft_findP_start(s1, set);
+//	tmpEnd = ft_findP_end(s1, set);
+//	return (ft_substr(s1, tmpStart, (tmpEnd - tmpStart)));
+//}
+
+/**
+ * /////NEW CODE!/////NEW CODE!/////NEW CODE!/////NEW CODE!/////NEW CODE!///////*
+*/
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	tmpStart;
-	size_t	tmpEnd;
+	char		*s;
+	size_t		start;
+	size_t		end;
 
-	tmpStart = 0;
-	tmpEnd = 0;
-	if (!s1)
+	if (!s1 || !set)
 		return (NULL);
-	if (ft_strlen(set) == 0)
-		return ((char *)s1);
-	tmpStart = ft_findP_start(s1, set);
-	tmpEnd = ft_findP_end(s1, set);
-	return (ft_substr(s1, tmpStart, (tmpEnd - tmpStart)));
+	end = ft_strlen(s1);
+	start = 0;
+	while ((start < end) && (s1[start] != '\0'))
+	{
+		if (ft_strchr(set, s1[start]))
+			start++;
+		else
+			break ;
+	}
+	while ((end > 0) && (s1[end - 1] != '\0'))
+	{
+		if (ft_strchr(set, s1[end - 1]))
+			end--;
+		else
+			break ;
+	}
+	s = ft_substr(s1, start, (end - start));
+	return (s);
 }
